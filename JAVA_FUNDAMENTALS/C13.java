945b82e88b447c452ef5528fe8f2c5331b330185
Bhupendarcho22 <bhupendar.d2328@gmail.com>
2025-07-15 20:58:28 +0530

    Add files via upload

---
+++ JAVA_FUNDAMENTALS/C13.java
@@ -0,0 +1,10 @@
+class C13{
+public static void main(String args[]){
+String str1=args[0];
+String str2=args[1];
+int c=Integer.parseInt(str1);
+int d=Integer.parseInt(str2);
+String result="The sum of "+c+" and "+d+" is "+ (c+d);
+System.out.println(result);
+}
+}
